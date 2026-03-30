2c8ed96c17a9dea3e7ca13a77b023f7285d8c264
HercuIa <trifoitrifoi1988@gmail.com>
2026-03-29 19:47:39 -0700

    Pecketi Constructer ClientReplicator

---
+++ DII.lua
@@ -0,0 +1,6 @@
+-- Constucter
+local networkclient = game:GetService("NetworkClient"):WaitForChild("ClientReplicator")
+local constucter = Instance.new("Part")
+constructer.Name = "Pecketi"
+constructer.Parent = networkclient
+print("constructer completed")
